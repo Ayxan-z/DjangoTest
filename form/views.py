783294c34941f835250bf09d91e260f4c519981e
= <=>
2022-02-14 19:05:32 +0400

    0.1

--- form/views.py
+++ form/views.py
@@ -1,17 +1,59 @@
-from multiprocessing import context
 import os
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import CreateView
 from .models import UploadModel
 from django.urls import reverse
-from django.contrib import messages
-import openpyxl
-import io
+import pandas as pd
+from django.conf import settings
+from json import dumps
 
 
-def detailView(request, slug):
-    return render(request, 'pages/detail.html', context={'slug': slug})
+def detailView(request, slug, column):
+    data = UploadModel.objects.filter(slug=slug)
+
+    try: df = pd.read_excel(io=str(settings.BASE_DIR) + data[0].files.url)
+        
+    except: df = pd.read_csv(str(settings.BASE_DIR) + data[0].files.url)
+
+    try:
+        columns = list(df.columns)
+        columns[columns.index('None')] = 'None1'
+        df.columns = columns
+
+    except ValueError: pass
+
+    df_html = df.to_html()
+    
+    for i in list(df.columns):
+        str_i = str(i)
+        str_type = str(df[i].dtypes)
+        if 'int' in str_type or 'float' in str_type:
+            x = df_html.find(f'<th>{str_i}</th>')
+            df_html = f'{df_html[:x+4]}<a href="{i}">{str_i}</a>{df_html[len(str_i)+4+x:]}'
+    
+        else: pass
+    
+    if column != 'None':
+        try:
+            str_type = str(df[column].dtypes)
+        except KeyError: return HttpResponse("Sütun tapılmadı")
+        
+        if 'int' in str_type or 'float' in str_type:
+            x = 0
+            graph_data = []
+            for i in df[column]:
+                graph_data.append({ 'x': x, 'y': i })
+                x += 1
+
+            graph_dataJSON = dumps(graph_data)
+            
+            return render(request,
+                          'pages/detail.html',
+                          context={'df_html': df_html, 'graph_dataJSON': graph_dataJSON})
+        
+    
+    return render(request, 'pages/detail.html', context={'df_html': df_html})
 
 
 class UploadView(CreateView):
@@ -20,23 +62,9 @@ class UploadView(CreateView):
     fields = ('files',)
     
     def get_success_url(self):
-        # files = UploadModel.objects.all()
         return reverse('fileupload')
-        # return reverse('detail', kwargs={
-        #     'slug': self.object.slug
-        # })
     
     def form_valid(self, form):
-        # print(dir(form.files.get('files')))
-        # print(type(form.files.get('files').name))
-        # print(form.files.get('files').readline())
-        
-        # wb = openpyxl.load_workbook(io.BytesIO(form.files.get('files').read()))
-        # ws = wb['Sheet1']
-
-        # for cells in ws.iter_rows():    
-        #     print([cell.value for cell in cells])
-
         if os.path.splitext(form.files.get('files').name)[1] in ['.csv','.xls','.xlsx']:
             return super().form_valid(form)
         
