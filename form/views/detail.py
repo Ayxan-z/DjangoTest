23bdce9b3e920cb34f99c3ac21dfecc0b4224d24
= <=>
2022-02-17 21:08:36 +0400

    0.5

--- form/views/detail.py
+++ form/views/detail.py
@@ -4,13 +4,20 @@ from .detail_validate import DetailValidate
 
 def detailView(request, slug, column_index):
     dv = DetailValidate()
-    table = dv.createTable(slug)
+    tables = dv.createTable()
+    # if type(table) != str: return table
     
-    if type(table) != str: return table
+    tables_HTML = []
+    for value in tables.values():
+        if value[0]:
+            tables_HTML.append(value[0])
+
+    graph = dv.createLineGraph(slug, column_index)
+    if not graph: graph = [None, None]
     
-    graph = dv.createLineGraph(column_index)
     return render(request,
                 'pages/detail.html',
-                 context={'df_html': table,
-                        'graph_dataJSON': graph})
+                 context={'tables': tables_HTML,
+                        'graph_dataJSON': graph[0],
+                        'graph_nameJSON': graph[1]})
         
--- form/views/detail_validate.py
+++ form/views/detail_validate.py
@@ -8,12 +8,12 @@ from json import dumps
 class DetailValidate():
     
     @staticmethod
-    def __openExcelAsDataFrame(data):
-        return pd.read_excel(io=str(settings.BASE_DIR) + data[0].files.url)
+    def __openExcelAsDataFrame(url) -> pd.DataFrame:
+        return pd.read_excel(io=str(settings.BASE_DIR) + url)
     
     @staticmethod
-    def __openCsvAsDataFrame(data) -> pd.DataFrame:
-        df = pd.read_csv(str(settings.BASE_DIR) + data[0].files.url)
+    def __openCsvAsDataFrame(url) -> pd.DataFrame:
+        df = pd.read_csv(str(settings.BASE_DIR) + url)
         cnt = 0
         d = {}
         values = df.values
@@ -36,7 +36,7 @@ class DetailValidate():
         return pd.DataFrame(d)
     
     @staticmethod
-    def __dataFrameToHTML(df):
+    def __dataFrameToHTML(df, slug):
         df_html = df.to_html()
         
         cnt = 1
@@ -45,7 +45,9 @@ class DetailValidate():
             str_type = str(df[i].dtypes)
             if 'int' in str_type or 'float' in str_type:
                 x = df_html.find(f'<th>{str_i}</th>')
-                df_html = f'{df_html[:x+4]}<a href="{str(cnt)}">{str_i}</a>{df_html[len(str_i)+4+x:]}'
+                df_html = f'{df_html[:x+4]}<a href="/detail/{slug}/{str(cnt)}">{str_i}</a>{df_html[len(str_i)+4+x:]}'
+                style_index = df_html.find('class="dataframe"')
+                df_html = df_html[:style_index+17]+' style="display: inline-block; margin-right: 10px;"'+df_html[style_index+17:]
         
             else: pass
             cnt += 1
@@ -62,26 +64,46 @@ class DetailValidate():
 
         return dumps(graph_data)
     
-    def createTable(self, slug):
-        data = UploadModel.objects.filter(slug=slug)
-        try: 
-            self.df = self.__openExcelAsDataFrame(data)
-
-        except: 
+    def createTable(self):
+        tables = UploadModel.objects.all()
+        # data = UploadModel.objects.filter(slug=slug)
+        
+        self.tables_data = {}
+        for table in tables:
             try: 
-                self.df = self.__openCsvAsDataFrame(data)
-                
-            except: return HttpResponse("<h2>Fayl oxunanmadı!</h2>")
+                df = self.__openExcelAsDataFrame(table.files.url)
+                df = [self.__dataFrameToHTML(df, table.slug), df]
 
-        df_html = self.__dataFrameToHTML(self.df)
-        return df_html
+            except: 
+                try: 
+                    df = self.__openCsvAsDataFrame(table.files.url)
+                    df = [self.__dataFrameToHTML(df, table.slug), df]
 
-    def createLineGraph(self, column_index) -> dumps or None:
-        if self.df.columns.__len__() >= column_index > 0 :
-            column_index -= 1
-            str_type = str(self.df[self.df.columns[column_index]].dtypes)
+                except: df = [None, None]
+
+            self.tables_data[table.slug] = df
+        return self.tables_data
+
+    def createLineGraph(self, slug, column_index) -> dumps or None:
+        if slug != 'None':
+            df = self.tables_data[slug][1]
+        
+            if df.columns.__len__() >= column_index > 0:
+                column_index -= 1
+                str_type = str(df[df.columns[column_index]].dtypes)
+
+                if 'int' in str_type or 'float' in str_type:
+                    graph_dataJSON = self.__dataFrameColumnToJSON(df, column_index)
+                    graph_nameJSON = dumps(df[df.columns[column_index]].name)
                     
-            if 'int' in str_type or 'float' in str_type:
-                graph_dataJSON = self.__dataFrameColumnToJSON(self.df, column_index)
-                return graph_dataJSON
+                    return [graph_dataJSON, graph_nameJSON]
+        
+        
+        # if self.df.columns.__len__() >= column_index > 0 :
+        #     column_index -= 1
+        #     str_type = str(self.df[self.df.columns[column_index]].dtypes)
+                    
+        #     if 'int' in str_type or 'float' in str_type:
+        #         graph_dataJSON = self.__dataFrameColumnToJSON(self.df, column_index)
+        #         return graph_dataJSON
             
